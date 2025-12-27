489739dfdeda933cc04f371f1e46053931cef058
Egor <egor-lih@mail.ru>
2025-12-27 12:01:23 +0700

    initialization

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { fileURLToPath, URL } from 'node:url';
+
+export default defineConfig({
+  base: '/frontend-project/',
+  server: {
+    host: '0.0.0.0', // Доступен извне для Live Share
+    port: 5173,
+    strictPort: true, // Ошибка, если порт занят
+  },
+  build: {
+    emptyOutDir: true,
+    modulePreload: { polyfill: false },
+    rollupOptions: {
+      input: {
+        index: resolve(fileURLToPath(new URL('.', import.meta.url)), 'index.html'),
+        lihodeevski: resolve(fileURLToPath(new URL('.', import.meta.url)), 'lihodeevski.html'),
+      },
+    },
+  },
+});
